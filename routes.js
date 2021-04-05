fa8c1945bcbc0d6fec454cc865c097fab76fbc4f
Gnong <jinhongdev@gmail.com>
2021-04-05 16:23:20 +0900

    Create Mongo DB Cluster

--- routes.js
+++ routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const productController = require('./controller/products');
 
-router.get('/', productControlle.hello);
+router.get('/', productController.hello);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const productRoutes = require('./routes');
 
 const PORT = 5000;
 
 const app = express();
 
-app.use("/api/products", (res, res), productRoutes);
+const dbpw = process.env.DBPASSWORD
+
+mongoose.connect('mongodb+srv://admin:gnong1475!@cluster-tdd.5hqkn.mongodb.net/Hello?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    seUnifiedTopology: true 
+}).then(() => console.log('MongoDB Connect..'))
+.catch(err => console.error(err))
+
+app.use("/api/products", productRoutes);
 
 app.use(express.json());
 app.get("/", (req, res) => {
