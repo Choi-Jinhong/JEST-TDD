35373454f848785b932b9cf8332b7822c44b85f9
Gnong <jinhongdev@gmail.com>
2021-04-05 18:54:04 +0900

    Add dotenv

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const productRoutes = require('./routes');
+const dotenv = require('dotenv');
 
 const PORT = 5000;
 
 const app = express();
+dotenv.config();
 
-const dbpw = process.env.DBPASSWORD
+const address = process.env.ADDRESS;
 
-mongoose.connect('mongodb+srv://admin:gnong1475!@cluster-tdd.5hqkn.mongodb.net/Hello?retryWrites=true&w=majority', {
+console.log(address);
+
+mongoose.connect(address, {
     useNewUrlParser: true,
-    seUnifiedTopology: true 
+    useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connect..'))
 .catch(err => console.error(err))
 
