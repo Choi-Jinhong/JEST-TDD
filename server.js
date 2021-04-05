191b3673da5807c7db21b05153fb8483caa6c6ce
Gnong <jinhongdev@gmail.com>
2021-04-05 15:56:03 +0900

    create route, controller

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const productRoutes = require('./routes');
 
 const PORT = 5000;
 
 const app = express();
+
+app.use("/api/products", (res, res), productRoutes);
 
 app.use(express.json());
 app.get("/", (req, res) => {
