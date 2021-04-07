bee961269f2659a3e30f0847203c9dceb3e1e732
Gnong <jinhongdev@gmail.com>
2021-04-07 12:05:27 +0900

    Create getProducts Unit Test(2)

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -2,6 +2,7 @@ const productController = require("../../controller/products");
 const productModel = require("../../models/Products");
 const httpMocks = require("node-mocks-http");
 const newProduct = require("../data/new-product.json");
+const allProducts = require("../data/all-products.json");
 
 productModel.create = jest.fn();
 productModel.find = jest.fn();
@@ -51,5 +52,15 @@ describe("Product Controller Get", () => {
   it("should call ProductModel.find()", async () => {
     await productController.getProducts(req, res, next);
     expect(productModel.find).toHaveBeenCalledWith({}); // find와 같은 함수를 사용할 때에는 미리 해당 함수를 jset.fn()으로 설정해줘야함
+  });
+  it("should retrun 200 response", async () => {
+    await productController.getProducts(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled).toBeTruthy();
+  });
+  it("should return json body in response", async () => {
+    productModel.find.mockReturnValue(allProducts);
+    await productController.getProducts(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(allProducts);
   });
 });
